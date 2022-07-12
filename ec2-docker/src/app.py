673fd720dd8cb4176e7add4e68a4664ba0bd7c87
VVeiZhang <34899541+vveizhang@users.noreply.github.com>
2022-07-11 22:15:48 -0500

    Update app.py

--- ec2-docker/src/app.py
+++ ec2-docker/src/app.py
@@ -6,8 +6,9 @@ st.write('This app uses Transformer model to identify the category of any input 
 st.markdown('The source code for this app can be found in this GitHub repo: ')
 
 example_text = """
-The US has ordered the relatives of its embassy staff in Ukraine to leave amid rising tension in the region. The State Department has also given permission for non-essential staff to leave and urged US citizens in Ukraine to consider departing. In a statement, it said there were reports that Russia is planning significant military action against Ukraine. Russia has denied claims that it is planning to invade Ukraine.
+GCATGTTGGCATTGAACATTGACGAAGCTATTACATTGCTTGAACAATTGGGACTTAGTGGCAGCTATCAATGGTGTAATACCACAGGATGGCATTCTACAAAGTGAATATGGAGGTGAGACCATACCAGGACCTGCATTTAATCCAGCAAGTCATCCAGCTTCAGCTCCTACTTCCTCTTCTTCTTCAGCGTTTCGACCTGTAATGCCATCCAGGCAGATTGTAGAAAGGCAACCTCGGATGCTGGACTTCAGGGTTGAATACAGAGACAGAAATGTTGATGTGGTACTTGAAGACACCTGTACTGTTG
 """
+
 
 input_text = st.text_area(
     label="Input/Paste News here:",
@@ -22,7 +23,7 @@ model = load_model(path="./model/transformer-model.pth")
 if input_text == "":
     input_text = example_text
 
-if st.button("Run GPT-2!"):
+if st.button("Run Transformer!"):
     if len(input_text) < 300:
         st.write("Please input more text!")
     else:
@@ -30,5 +31,4 @@ if st.button("Run GPT-2!"):
 
             model_input = read_input(input_text)
             model_output = run_model(model, *model_input)
-            st.write("Predicted News Category (with Probability):")
             st.write(model_output)
