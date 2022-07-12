7b4ab98e0923bd461a4277b2bff9ebca09822b70
VVeiZhang <34899541+vveizhang@users.noreply.github.com>
2022-07-12 13:17:53 -0500

    Update app.py

--- ec2-docker/src/app.py
+++ ec2-docker/src/app.py
@@ -33,4 +33,5 @@ if st.button("Run Transformer!"):
             model_input = read_input(input_text)
             model_output = run_model(model_input, model)
             predicted_label = 'circRNA' if model_output == 1.0 else 'lincRNA'
+            st.write(model_output)
             st.write(predicted_label)
